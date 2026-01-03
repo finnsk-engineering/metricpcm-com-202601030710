234b8cb955d4efa80a56c292e67f50c9c48a1e43
finnsk-engineering <finn@finnsk.com>
2026-01-03 18:34:47 +1100

    feat: deployed generated page ScottFletcher.jsx

---
+++ src/pages/ScottFletcher.jsx
@@ -0,0 +1,18 @@
+import TextBlock from "../components/TextBlock.jsx";
+
+const ScottFletcher = () => {
+  const section1Data = {
+    "type": "TextBlock",
+    "heading": "",
+    "body": "Scott Fletcher has extensive Project Supervisor experience from working in the role over the past 10 years. His experience spans multiple metalliferous sites in Central West, NSW in both ore processing and underground operations.\n\nScott provides a high level of experience with CMMS systems enabling detailed management on procurement, logistics and project data integration. Coupled with a fully developed electrical, hazardous areas trade and supervision background, he is an invaluable addition to any project team.",
+    "images": []
+  };
+
+  return (
+    <>
+      <TextBlock data={section1Data} />
+    </>
+  );
+};
+
+export default ScottFletcher;
